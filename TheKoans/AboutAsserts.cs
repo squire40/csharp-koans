4867e68662f022672fbc91e0201d17393928de5d
dbourke <davidbourke@quickenloans.com>
2018-08-27 15:08:02 -0400

    Initial commit

---
+++ TheKoans/AboutAsserts.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TheKoans
+{
+    [TestClass]
+    public class AboutAsserts
+    {
+        private static readonly Int32 FILL_ME_IN = new Int32();
+        private static readonly bool FILL_ME__IN;
+
+        [TestMethod]
+        public void AssertTruth()
+        {
+            Assert.IsTrue(FILL_ME__IN);   // Your long journey begins with a simple step.  We seek what's true, help us find it.
+        }
+
+        [TestMethod]
+        public void AssertsShouldHaveMessages()
+        {
+            Assert.IsTrue(FILL_ME__IN, "This message should help you understand what failed. Please help us find truth.");
+        }
+
+        [TestMethod]
+        public void AssertFalse()
+        {
+            Assert.IsFalse(true, "Truth comes in many forms. Here what is true should be false.");
+        }
+
+        [TestMethod]
+        public void AssertEquality()
+        {
+            Assert.IsTrue(1 == FILL_ME_IN, "Truth can be uncovered in boolean expressions when they are true.");
+        }
+
+        [TestMethod]
+        public void AssertEqualityTheBetterWay()
+        {
+            var expectedValue = FILL_ME_IN;
+            var actualValue = 1 + 1;
+
+            Assert.AreEqual(expectedValue, actualValue, "When your karma is broken it is more helpful to know what was expected and what it actually is.");
+        }
+
+        [TestMethod]
+        public void AssertFail()
+        {
+            bool thePathToEnlightment = FILL_ME__IN;
+            if (!thePathToEnlightment)
+            {
+                Assert.Fail("Taking an unfortunate code path breaks your karma. Change the path towards truth.");
+            }
+            
+            Assert.IsTrue(thePathToEnlightment, "The path has been found.");
+        }
+    }
+}
---
+++ TheKoans/AboutNull.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TheKoans
+{
+    // In Corey's Ruby Koans, this is called AboutNil. We've renamed it to AboutNull to match the C# convention
+    [TestClass]
+    public class AboutNull : KoanHelper
+    {
+        [TestMethod]
+        public void NilIsNotAnObject()
+        {
+            //not everything is an object
+            Assert.IsTrue(typeof(object).IsAssignableFrom(null), "To quote from South Park's World of Warcraft episode: 'Can you kill that which has no life?'"); 
+        }
+
+        [TestMethod]
+        public void NullIsNotAnInstanceOfObjectAndInvokingAMethodThrowsNullReferenceException()
+        {
+            Object nullReference = null;
+
+            try
+            {
+                nullReference.GetHashCode();
+            }
+            catch (Exception exception)
+            {
+                Assert.AreEqual(FILL_ME_IN, exception.Message, "If you're still unsure, take a look at the name of this method...");
+            }
+        }
+
+        [TestMethod]
+        public void CheckingThatAnObjectIsNull()
+        {
+            object obj = null;
+            Assert.IsTrue(obj == FILL_ME_IN);
+        }
+
+        [TestMethod]
+        public void ABetterWayToCheckThatAnObjectIsNull()
+        {
+            object obj = null;
+            Assert.IsNull(FILL_ME_IN, "If only we had an object passed in as a parameter...");
+        }
+
+        [TestMethod]
+        public void AWayNotToCheckThatAnObjectIsNull()
+        {
+            object obj = null;
+            Assert.IsTrue(obj.Equals(null));
+        }
+
+
+        // RJG Begin Under Construction
+
+        // Passing Null Objects to C# Extension Methods... is allowed?!?!?
+        // http://www.peteonsoftware.com/index.php/2012/06/21/c-extension-methods-on-null-objects/
+
+        // RJG End
+    }
+}
