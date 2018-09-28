373ab7f60e3e103213d39da60aa6d248e220be2d
Dave Bourke <squire40@hotmail.com>
2018-09-27 21:04:22 -0400

    AboutControlStatements

--- TheKoans/AboutControlStatements.cs
+++ TheKoans/AboutControlStatements.cs
@@ -20,7 +20,7 @@ namespace TheKoans
                 b = false;
             }
 
-            Assert.AreEqual(FILL_ME_IN, b, "To b or not to b -- that really is the answer...");
+            Assert.AreEqual(true, b, "To b or not to b -- that really is the answer...");
         }
 
         [TestMethod]
@@ -32,7 +32,7 @@ namespace TheKoans
             else
                 b = false;
 
-            Assert.AreEqual(FILL_ME_IN, b, "Unless the statement was break, continue or return, I would always use brackets to avoid confusion.");
+            Assert.AreEqual(true, b, "Unless the statement was break, continue or return, I would always use brackets to avoid confusion.");
 
         }
 
@@ -45,7 +45,7 @@ namespace TheKoans
                 b = true;
             }
 
-            Assert.AreEqual(FILL_ME_IN, b, "Brackets do not cause a racket; they provide clarity and potential for future statements.");
+            Assert.AreEqual(true, b, "Brackets do not cause a racket; they provide clarity and potential for future statements.");
         }
 
         [TestMethod]
@@ -55,7 +55,7 @@ namespace TheKoans
             if (true)
                 b = true;
 
-            Assert.AreEqual(FILL_ME_IN, b, "The code may look cleaner without them, but brackets help avoid confusion.");
+            Assert.AreEqual(true, b, "The code may look cleaner without them, but brackets help avoid confusion.");
         }
 
         [TestMethod]
@@ -70,15 +70,15 @@ namespace TheKoans
                 b1 = true;
                 b2 = true;
 
-            Assert.AreEqual(FILL_ME_IN, b1, "Indenting usually makes your code more readable, but only when used correctly..");
-            Assert.AreEqual(FILL_ME_IN, b2, "Whitespace is not always your friend.. Brace yourselves!");
+            Assert.AreEqual(false, b1, "Indenting usually makes your code more readable, but only when used correctly..");
+            Assert.AreEqual(true, b2, "Whitespace is not always your friend.. Brace yourselves!");
         }
 
         [TestMethod]
         public void TernaryOperators()
         {
-            Assert.AreEqual(FILL_ME_IN, (true ? 1 : 0), "It is now your tern, grasshopper, to choose the right value.");
-            Assert.AreEqual(FILL_ME_IN, (false ? 1 : 0), "Or perhaps you will intern here until you choose correctly..?");
+            Assert.AreEqual(1, (true ? 1 : 0), "It is now your tern, grasshopper, to choose the right value.");
+            Assert.AreEqual(0, (false ? 1 : 0), "Or perhaps you will intern here until you choose correctly..?");
         }
 
         //This is out of place for control statements, but necessary to understand AssignIfNullOperator below
@@ -90,8 +90,8 @@ namespace TheKoans
             int? nullableInt = null; //but you can do this
             int j;  // But what about this?
 
-            Assert.AreEqual(FILL_ME_IN, i == null, "This has the same effect as using: int i = new int();");
-            Assert.AreEqual(FILL_ME_IN, nullableInt == null, "It looks odd to see int? -- as if the developer wasn't sure? -- but that's exactly the point!");
+            Assert.AreEqual(false, i == null, "This has the same effect as using: int i = new int();");
+            Assert.AreEqual(true, nullableInt == null, "It looks odd to see int? -- as if the developer wasn't sure? -- but that's exactly the point!");
             //Assert.AreEqual(FILL_ME_IN, j == null, "Using uninitialized variables, even to check its value, is not allowed.");
         }
 
@@ -102,7 +102,7 @@ namespace TheKoans
 
             int x = nullableInt ?? 42;
 
-            Assert.AreEqual(FILL_ME_IN, x, "Figuring this out is like trying to figure out the meaning of life...");
+            Assert.AreEqual(42, x, "Figuring this out is like trying to figure out the meaning of life...");
         }
 
         [TestMethod]
@@ -124,9 +124,9 @@ namespace TheKoans
                 isAboutControlStatements = true;
 
             // If you have ReSharper, don't mouse over the squiggly lines!
-            Assert.AreEqual(FILL_ME_IN, isAboutMethods, "As a boolean, hopefully you have two chances of getting this right."); 
-            Assert.AreEqual(FILL_ME_IN, isKoan, "As a Koan, it isn't a matter of getting these right; it's a matter of learning.");
-            Assert.AreEqual(FILL_ME_IN, isAboutControlStatements, "As a matter of learning, there is always a chance of learning more.");
+            Assert.AreEqual(false, isAboutMethods, "As a boolean, hopefully you have two chances of getting this right."); 
+            Assert.AreEqual(true, isKoan, "As a Koan, it isn't a matter of getting these right; it's a matter of learning.");
+            Assert.AreEqual(true, isAboutControlStatements, "As a matter of learning, there is always a chance of learning more.");
 
 
         }
@@ -141,7 +141,7 @@ namespace TheKoans
                 result = result * i;
                 i += 1;
             }
-            Assert.AreEqual(FILL_ME_IN, result, "If IT was only a couple centuries younger, this method might be a WhilstStatement, which might seem odd, I suppose.");
+            Assert.AreEqual(120, result, "If IT was only a couple centuries younger, this method might be a WhilstStatement, which might seem odd, I suppose.");
         }
 
         [TestMethod]
@@ -154,7 +154,7 @@ namespace TheKoans
                 result = result*i;
                 i += 1;
             } while (i <= 5);
-            Assert.AreEqual(FILL_ME_IN, result, "Logically, this makes more sense to me. One has to do work first before seeing if they have to stop.");
+            Assert.AreEqual(120, result, "Logically, this makes more sense to me. One has to do work first before seeing if they have to stop.");
         }
 
         [TestMethod]
@@ -168,7 +168,7 @@ namespace TheKoans
                 result = result * i;
                 i += 1;
             }
-            Assert.AreEqual(FILL_ME_IN, result, "Awww - Break out! Le Break: c'est chic!");
+            Assert.AreEqual(120, result, "Awww - Break out! Le Break: c'est chic!");
         }
 
         [TestMethod]
@@ -182,7 +182,7 @@ namespace TheKoans
                 if ((i % 2) == 0) { continue; }
                 result += i;
             }
-            Assert.AreEqual(FILL_ME_IN, result, "Don't let the math scare your Karma away.. Persevere!");
+            Assert.AreEqual(25, result, "Don't let the math scare your Karma away.. Persevere!");
         }
 
         [TestMethod]
@@ -193,7 +193,7 @@ namespace TheKoans
             {
                 list[i] = (list[i].ToUpper());
             }
-            CollectionAssert.AreEqual(new[] { FILL_ME_IN }, list, "Converting the values while parsing through them sounds dangerous, eh?");
+            CollectionAssert.AreEqual(new[] { "FISH", "AND", "CHIPS" }, list, "Converting the values while parsing through them sounds dangerous, eh?");
         }
 
         [TestMethod]
@@ -205,8 +205,8 @@ namespace TheKoans
             {
                 finalList.Add(item.ToUpper());
             }
-            CollectionAssert.AreEqual(new[] { FILL_ME_IN }, list, "As source data goes, this really just needs some tartar sauce.");
-            CollectionAssert.AreEqual(new[] { FILL_ME_IN }, finalList, "I SAID, 'AS SOURCE DATA GOES, THIS REALLY JUST NEEDS SOME TARTAR SAUCE!'");
+            CollectionAssert.AreEqual(new[] { "fish", "and", "chips" }, list, "As source data goes, this really just needs some tartar sauce.");
+            CollectionAssert.AreEqual(new[] { "FISH", "AND", "CHIPS" }, finalList, "I SAID, 'AS SOURCE DATA GOES, THIS REALLY JUST NEEDS SOME TARTAR SAUCE!'");
 
             // For more information, check http://msdn.microsoft.com/en-us/library/ttw7t8t6.aspx
         }
@@ -224,7 +224,7 @@ namespace TheKoans
             }
             catch (Exception ex)
             {
-                Assert.AreEqual(typeof(FILL_ME_IN), ex.GetType(), "Messing with the Collection during a foreach enumeration seems wrong.. invalid even.");
+                Assert.AreEqual(typeof(InvalidOperationException), ex.GetType(), "Messing with the Collection during a foreach enumeration seems wrong.. invalid even.");
             }
         }
 
@@ -253,7 +253,7 @@ namespace TheKoans
                 whoCaughtTheException = "When we tried to move to the next item in the list";
             }
 
-            Assert.AreEqual(FILL_ME_IN, whoCaughtTheException, "If you can solve this whodunit mystery, your Karma will thank you.");
+            Assert.AreEqual("When we tried to move to the next item in the list", whoCaughtTheException, "If you can solve this whodunit mystery, your Karma will thank you.");
         }
 
     }
